eb1511f270d48a20ea83b96da1d077fe8664e47c
VarozXYZ <alvaropiro@gmail.com>
2026-01-20 23:48:01 +0100

    project foundation with tailwind and routing

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Routes, Route } from 'react-router-dom'
+
+function App() {
+    return (
+        <Routes>
+            <Route path="/" element={<div className="text-accent font-sora text-4xl p-8">Didactio - Coming Soon</div>} />
+        </Routes>
+    )
+}
+
+export default App
